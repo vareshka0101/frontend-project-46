94edffbfeb08ed0e42b4a43ab6fa025c2256b2ff
vareshka0101 <vareshka0101@mail.ru>
2024-07-16 23:34:29 +0300

    add file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,25 @@
-import program from 'commander';
-import { parseFile } from './parsers';
+#!/usr/bin/env node
+
+import fs from 'fs';
+import path from 'path';
+import { Command } from 'commander';
+import { parseFile, compareObjects } from '../src/parsers.js';
+
+const program = new Command();
 
 program
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const file1 = parseFile(filepath1);
-    const file2 = parseFile(filepath2);
+    const data1 = parseFile(filepath1);
+    const data2 = parseFile(filepath2);
+
+    console.log('File 1 data:', data1);
+    console.log('File 2 data:', data2);
+
+    const differences = compareObjects(data1, data2);
+    console.log('Differences:', differences);
   });
 
 program.parse(process.argv);
+
