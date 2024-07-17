a972ce7ab4ee79cbe35a931bde112d3563dbdc03
vareshka0101 <vareshka0101@mail.ru>
2024-07-17 15:14:12 +0300

    update file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,7 @@
 import fs from 'fs';
 import path from 'path';
 import { Command } from 'commander';
-import { parseFile, compareObjects } from '../src/parsers.js';
+import { genDiff } from '../parsers.js';
 
 const program = new Command();
 
@@ -11,15 +11,8 @@ program
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const data1 = parseFile(filepath1);
-    const data2 = parseFile(filepath2);
-
-    console.log('File 1 data:', data1);
-    console.log('File 2 data:', data2);
-
-    const differences = compareObjects(data1, data2);
-    console.log('Differences:', differences);
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
   });
 
 program.parse(process.argv);
-
